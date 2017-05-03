c9f580f3fb64d3cbeec35088c130070658ac9578
Timon <timon.mehrling@desy.de>
2017-05-03 10:27:55 +0200

    Now forwarding all arguments passed to bash script to python script.

--- raw-slice-series.sh
+++ raw-slice-series.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-if [ "$#" -eq 0 ]; then
-  echo "No path specified, using ./DATA..." 
-  DATA_PATH="DATA"
-elif [ "$#" -eq 1 ]; then
-  DATA_PATH=$1
-else 
-  echo "Illegal number of parameters!"    
-fi
-
-nohup raw-slice-series.py $DATA_PATH 1> rss.out 2> rss.err &
+nohup raw-slice-series.py "$@" 1> rss.out 2> rss.err &
