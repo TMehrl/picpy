0fea72491f26ad7e2975ae34b878d896daed9250
Timon Mehrling <t.mehrling@gmail.com>
2018-02-14 17:50:56 -0800

    Forgot to initialize array (should really test things locally, but anyway...).

--- plot_g3d.py
+++ plot_g3d.py
@@ -592,6 +592,7 @@ class G3d_plot_line(G3d_plot):
 
         if self.args.range != None:
             ax.set_xlim(self.args.range)
+            idx = np.zeros(2)
             idx[0] = (np.abs(self.x_array-self.args.range[0])).argmin()
             idx[1] = (np.abs(self.x_array-self.args.range[1])).argmin()
             idx = numpy.sort(idx)
