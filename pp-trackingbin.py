4884c5620b2706b1593feed7bd4b8a3054d84957
Severin Diederichs <severin.diederichs@desy.de>
2018-06-11 16:07:30 -0700

    Particle tracking tools

--- pp-trackingbin.py
+++ pp-trackingbin.py
@@ -61,7 +61,34 @@ def binSlab_parser():
                           action="store_true",
                           default=True,
                           help = "If data is w/o halo (default: %(default)s).")                                                       
+    parser.add_argument(  "--2Dproj",
+                          dest = "twodproj",
+                          default = False,
+                          help = "Plots a 2D projection of the particle" 
+                          "trajectories instead of full 3D plot. \n"
+                          "Use only if acquired data is based on a single slice! (default: %(default)s).")
+
+
     return parser
+
+
+def plot_3D_colourline(x,y,z,c, minc, maxc):
+    
+    c = cm.jet((c-minc)/(maxc-minc))
+    ax = plt.gca()
+    for i in np.arange(len(x)-1):
+        ax.plot([x[i],x[i+1]], [y[i],y[i+1]], [z[i],z[i+1]], c=c[i])
+    
+    return
+    
+def plot_2D_colourline(x,z,c, minc, maxc):
+    
+    c = cm.jet((c-minc)/(maxc-minc))
+    ax = plt.gca()
+    for i in np.arange(len(x)-1):
+        ax.plot([x[i],x[i+1]], [z[i],z[i+1]], c=c[i])
+    
+    return
 
 
 def main():
@@ -70,106 +97,131 @@ def main():
     args = parser.parse_args()
 
     # NHC=2
-    slice_suffix_str = '_izeta_'
-
-
-    # grid_info = np.zeros((3, 3))
+    proc_suffix_str = '_proc_'
+    ppart_track_str = 'ppart_track'
+    bin_fending = '.bin'
     
-    # with open(args.outsumfile, 'r') as csvfile:
-    #     csvread = csv.reader(csvfile, delimiter='\t')
-    #     i=0
-    #     for line in csvread:
-    #         if (i<3):
-    #             for j in range(len(line)):
-    #                 grid_info[i,j] = float(line[j])
-    #         i += 1        
-
-    # print(grid_info)
-
-    # Nx = int(grid_info[1,2])
-    # Ny = int(grid_info[2,2])
-
-    # dx=(grid_info[1,1] - grid_info[1,0]) / (Nx - 1) # Check this!!!!
-    # dy=(grid_info[2,1] - grid_info[2,0]) / (Ny - 1) # Check this!!!!
-
-    # Nx_incl_halo = Nx + 2*NHC;
-    # Ny_incl_halo = Ny + 2*NHC;
-
-
     for filepath in args.path:
-
-        # if args.nohalo:
-        #     nhc = 0
-        # else:
-        #     nhc = NHC
-
-        i_sl = 0 #np.int(filepath[(filepath.find(slice_suffix_str)+len(slice_suffix_str)):len(filepath)])
-        print(i_sl)
-        # IDx = np.int(np.floor(slID/args.partition[2]))
-        # IDy = slID - args.partition[2] * IDx
-
-        # ix_start = np.int((Nx * IDx/args.partition[1]))
-        # ix_end = np.int((Nx * (IDx+1)/args.partition[1]))
-
-        # iy_start = np.int((Ny * IDy/args.partition[2]))
-        # iy_end = np.int((Ny * (IDy+1)/args.partition[2]))
-
-        # my_Nx=np.int(Nx/args.partition[1] + 2*nhc )
-        # my_Ny=np.int(Ny/args.partition[2] + 2*nhc )
-    
-        # my_x_array = np.linspace( grid_info[1,0] + ix_start*dx - nhc*dx, 
-        #                           grid_info[1,0] + ix_end*dx + nhc*dx,
-        #                           my_Nx)
-        # my_y_array = np.linspace( grid_info[2,0] + iy_start*dy - nhc*dy,
-        #                           grid_info[2,0] + iy_end*dy + nhc*dy,
-        #                           my_Ny)
-
+        
+        
+        
+        i_sl = np.int(filepath[(filepath.find(proc_suffix_str)+len(proc_suffix_str)):(len(filepath)-len(bin_fending))])
+        #print(i_sl)
+        
         if i_sl == 0:
-
-            filepath_base = filepath[0:(filepath.find(slice_suffix_str)+len(slice_suffix_str))]
+        
+            filepath_base = filepath[0:(filepath.find(proc_suffix_str)+len(proc_suffix_str))]
+            #print(filepath_base)
             Nsl = len(glob.glob(filepath_base + '*'))
-
+            #print(Nsl)
+        
             array = np.empty(0)
             for i in range(0,Nsl):
-                filepath_slice = '%s%04d.bin' % (filepath_base, i )
+                filepath_slice = '%s%d%s' % (filepath_base, i, bin_fending )
                 print('Reading: %s' % filepath_slice)
                 array = np.append(array,np.fromfile(filepath_slice,dtype=np.float32))
-            
-            array = np.reshape(array, (int(len(array)/7),7))
-            print(array)
+        
+            # filepath_base = filepath
+            # array = np.empty(0)
+            # for files in glob.glob(filepath_base + '/*'):
+            #     #filepath_slice = '%s%d.bin' % (filepath_base, i )
+            #     print('Reading: %s' % files)
+            #     array = np.append(array,np.fromfile(files,dtype=np.float32))
+        
+            ''' reshape the binary data to a matrix with particle
+            tag information in each row'''
+            array = np.reshape(array, (int(len(array)/10),10))
             print(np.shape(array))
-            
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            x=array[:,0]
-            y=array[:,1]
-            z=np.arange(len(array))
-            print(len(x))
-            print(len(y))
-            print(len(z))
-            ax.plot(x, z, label='x')
-            ax.plot(y,z, label='y')
-            ax.legend()
-            
-            plt.show()
-            # fig = plt.figure()
-            # cax = plt.plot( array)
-            # 
-            # ax = plt.gca()
-            # ax.set_ylabel(r'$\eta (\zeta)$', fontsize=14)
-            # ax.set_xlabel(r'$k_p \zeta$', fontsize=14)
-            # 
-            # if args.ifshow:
-            #   plt.show()
-            # 
-            # head, savename = os.path.split(filepath)
-            # 
-            # mkdirs_if_nexist(args.savepath)
-            # 
-            # print('Saving: ' + args.savepath + '/' + savename + '.eps')
-            # 
-            # fig.savefig( args.savepath + '/' + savename + '.eps',
-            #         format='eps')    
+            ''' set up figure for 3D plotting '''
+        
+        
+            ''' In order to simplify the splitting and plotting, the particle
+            information matrix is sorted by the plasma species, the proc tag, then by the particle tag
+            and finally by zeta '''
+            indizes = np.lexsort((array[:,5], array[:,6], array[:,7], array[:,9])) 
+            array = array[indizes]
+        
+            ''' split by different plasma species types '''
+            w = np.split(array, np.where(np.diff(array[:, 9]) != 0)[0]+1) 
+        
+            for k in range(len(w)):
+        
+        
+                fig = plt.figure()
+                if args.twodproj:
+                    ax = fig.add_subplot(111)
+                else:
+                    ax = fig.add_subplot(111, projection='3d')
+                ''' get min and max value for universal colorbar later '''
+                cmin = min(w[k][:,8])
+                cmax = max(w[k][:,8])
+        
+                ''' Splitting the array into each particle trajectory by proc tag'''
+                d= np.split(w[k], np.where(np.diff(w[k][:, 7]) != 0)[0]+1) 
+        
+                for i in range(len(d)):
+        
+                    ''' Splitting the array into each particle trajectory by part tag'''
+                    e = np.split(d[i], np.where(np.diff(d[i][:, 6]) != 0)[0]+1) 
+        
+                    for j in range(len(e)):
+                        x=e[j][:,0]
+                        y=e[j][:,1]
+                        z=e[j][:,5]
+                        c=e[j][:,8]
+                        # print(len(x))
+                        # print(len(y))
+                        print("laenge track proc tag %i part tag %i ist %i" %(i, j, len(z)))
+                        
+                        if args.twodproj:
+                            plot_2D_colourline(z,x,c, cmin, cmax)
+                        else:
+                            plot_3D_colourline(z,y,x,c, cmin, cmax)
+                        
+                        #ax.plot(z, y, x, label='particle track')
+        
+                ''' Set colorbar ''' 
+                norm = matplotlib.colors.Normalize(
+                vmin=np.min(cmin),
+                vmax=np.max(cmax))
+                # choose a colormap
+                c_m = cm.jet
+                # create a ScalarMappable and initialize a data structure
+                s_m = matplotlib.cm.ScalarMappable(cmap=c_m, norm=norm)
+                s_m.set_array([])
+        
+                cbar = plt.colorbar(s_m)
+                cbar.ax.set_ylabel(r'$|u|$')
+        
+                ax.set_xlim(0, 300)
+                ax.set_ylim(-6, 6)
+                if not args.twodproj:
+                    ax.set_zlabel(' x ')
+                    ax.set_zlim(-6, 6)
+                ax.set_xlabel(r'$\zeta$')
+                ax.set_ylabel(' y ')
+                
+                #ax.legend()
+        
+                plt.show()
+        #     # fig = plt.figure()
+        #     # cax = plt.plot( array)
+        #     # 
+        #     # ax = plt.gca()
+        #     # ax.set_ylabel(r'$\eta (\zeta)$', fontsize=14)
+        #     # ax.set_xlabel(r'$k_p \zeta$', fontsize=14)
+        #     # 
+        #     # if args.ifshow:
+        #     #   plt.show()
+        #     # 
+        #     # head, savename = os.path.split(filepath)
+        #     # 
+        #     # mkdirs_if_nexist(args.savepath)
+        #     # 
+        #     # print('Saving: ' + args.savepath + '/' + savename + '.eps')
+        #     # 
+        #     # fig.savefig( args.savepath + '/' + savename + '.eps',
+        #     #         format='eps')    
 
 if __name__ == "__main__":
     main()  
