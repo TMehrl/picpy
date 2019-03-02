bc9073468e60931e87f7249d8dfbb05fbbf67a69
Timon Mehrling <t.mehrling@gmail.com>
2019-03-01 17:41:35 -0800

    Improved slurm batch submission script.

--- exe-scripts/slurm_batch.sh
+++ exe-scripts/slurm_batch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -l
 
-#SBATCH --partition=debug
+## may be started using
+## sbatch --dependency=afterok:<jobID_A> rss_batch.sh 
+
+#SBATCH --partition=regular
 #SBATCH --nodes=1
-#SBATCH --time=00:30:00
+#SBATCH --time=06:00:00
 #SBATCH --license=SCRATCH
 #SBATCH --job-name=rss
 #SBATCH --output=rss-%j-%N.out
@@ -11,6 +14,7 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=tjmehrling@lbl.gov
 
-srun -n 1 -c 4 \
-	python3 $PP_PATH/picpy.py \
-	rss --zeta-range -2 0 -o 4 -p 4 DATA/raw_witness_beam_*
+Nproc=12
+
+srun -n 1 -c $Nproc \
+	python3 $PP_PATH/picpy.py rss --zeta-range -2 0 -o 4 -p $Nproc DATA/raw_witness_beam_*
